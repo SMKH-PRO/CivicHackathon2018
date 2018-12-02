1212a1772453622a0112531316a2a5fea302f6f9
KASHAN HAIDER <41765372+SMKH-PRO@users.noreply.github.com>
2018-12-02 15:57:28 +0500

    Initial commit

---
+++ precache-manifest.5b157679651c5427720d7d8eb5c7e4c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "006b8f740650ffb7d729",
+    "url": "/static/js/main.006b8f74.chunk.js"
+  },
+  {
+    "revision": "7215957c1390226be592",
+    "url": "/static/js/1.7215957c.chunk.js"
+  },
+  {
+    "revision": "006b8f740650ffb7d729",
+    "url": "/static/css/main.ff286b19.chunk.css"
+  },
+  {
+    "revision": "2a882dcafa80f45871286963fabfb706",
+    "url": "/index.html"
+  }
+];
